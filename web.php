1a4027fbf6c0b55dee499d89d1b6b65bec7de96f
septo_danu <146683390+danuuamn@users.noreply.github.com>
2025-05-18 17:20:08 +0700

    Update web.php

--- web.php
+++ web.php
@@ -13,7 +13,7 @@
 <body>
   <nav class="navbar navbar-expand-lg">
     <div class="container d-flex justify-content-center">
-      <a class="navbar-brand" href="#">AUDIORA</a>
+      <a class="navbar-brand" href="web.php">AUDIORA</a>
       
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,46 +23,46 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
         <ul class="navbar-nav ms-lg-auto me-lg-5 mb-2 mb-lg-0 align-items-lg-center">
-<li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Categories
-  </a>
-  <ul class="dropdown-menu">
-    <li class="dropdown-submenu">
-      <a class="dropdown-item dropdown-toggle" href="#">Earphone</a>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Bluetooth Earphone</a></li>
-        <li><a class="dropdown-item" href="#">Wired Earphone</a></li>
-        <li><a class="dropdown-item" href="#">Bluetooth Headphone</a></li>
-      </ul>
-    </li>
-    <li>
-      <a class="dropdown-item" href="#">Speaker</a>
-    </li>
-  </ul>
-</li>
 
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categories
+            </a>
+            <ul class="dropdown-menu">
+              <li class="dropdown-submenu">
+                <a class="dropdown-item dropdown-toggle" href="#">Earphone</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="bt_earphone.php">Bluetooth Earphone</a></li>
+                  <li><a class="dropdown-item" href="wr_earphone.php">Wired Earphone</a></li>
+                  <li><a class="dropdown-item" href="bt_headphone.php">Bluetooth Headphone</a></li>
+
+                </ul>
+              </li>
+                <a class="dropdown-item" href="speaker.php">Speaker</a>
+              </li>
+            </ul>
+          </li>
 
 
           <li class="nav-item">
-            <a class="nav-link" href="#">About us</a>
+            <a class="nav-link" href="about.php">About us</a>
           </li>
 
-              <li class="nav-item dropdown">
+          <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Login
             </a>
             <ul class="dropdown-menu">
               <li class="dropdown-submenu">
                 <li class="nav-item">
-                  <a class="nav-link" href="login.php">
+                  <a class="drop-login" href="login.php">
                     <i class="bi bi-person me-1"></i>Sign In
                   </a>
                 </li>
               </li>
               <li class="dropdown-submenu">
                 <li class="nav-item">
-                  <a class="nav-link" href="register.php">
+                  <a class="drop-login" href="register.php">
                     <i class="bi bi-person me-1"></i>Sign Up
                   </a>
                 </li>
@@ -88,9 +88,9 @@
   </nav>
   <div class="full-banner">
  <img src="R47.png" alt="Banner R47" class="banner-img" />
- <a href="#produk" class="banner-button">Lihat Selengkapnya</a>
+ <a href="bt_earphone.php" class="banner-button">Lihat Selengkapnya</a>
 </div>
- <div class="the-latest">TERBARU</div>
+ <div class="the-latest">TERBARU</div> 
 
 <div class="carousel-container">
   <button class="nav-button nav-prev" onclick="scrollCarousel(-1)">&#10094;</button>
@@ -124,44 +124,58 @@
 <div class="the-latest">  TERLARIS </div>
 
 
-  <div class="product-list">
+<div class="product-list">
+  <a href="bt_earphone.php" class="text-decoration-none text-dark">
     <div class="product-card">
       <img src="3pro.png" alt="pro2">
       <p class="title">Audiora earbuds 3 pro</p>
       <p class="price">Rp. 3,099,000</p>
       <p class="rating">★★★★☆</p>
     </div>
+  </a>
+
+  <a href="bt_headphone.php" class="text-decoration-none text-dark">
     <div class="product-card">
       <img src="airdora.png" alt="air">
       <p class="title">Airdora Max Wireless</p>
       <p class="price">Rp. 6,399,000</p>
       <p class="rating">★★★☆☆</p>
     </div>
+  </a>
+
+  <a href="speaker.php" class="text-decoration-none text-dark">
     <div class="product-card">
       <img src="2black.png" alt="Speaker">
       <p class="title">Audiora 2 maxi</p>
       <p class="price">Rp. 9,399,000</p>
       <p class="rating">★★★★☆</p>
     </div>
+  </a>
+
+  <a href="bt_headphone.php" class="text-decoration-none text-dark">
     <div class="product-card">
       <img src="h7.png" alt="game">
       <p class="title">Indora H7 Wireless</p>
       <p class="price">Rp. 8,999,000</p>
       <p class="rating">★★★★☆</p>
     </div>
-  </div>
+  </a>
 </div>
 
-<div class="the-latest"> OFFERS </div>
-<div class="product-card-horizontal">
-  <img src="reg.png" alt="offer" class="product-image">
-  <div class="product-info">
-    <p class="price">Dapatkan benefitnya</p>
-    <p class="title">Register ke Audiora sekarang!</p>
-    <p class="description">Daftarkan Membership Audiora Anda. Dapatkan berbagai keuntungan eksklusif yang tidak bisa Anda temuin di tempat lain.</p>
+<a href="register.php">
+  <div class="the-latest"> OFFERS </div>
+  <div class="product-card-horizontal">
+    <img src="reg.png" alt="offer" class="product-image">
+    <div class="product-info">
+      <p class="price">Dapatkan benefitnya</p>
+      <p class="title">Register ke Audiora sekarang!</p>
+      <p class="description">Daftarkan Membership Audiora Anda. Dapatkan berbagai keuntungan eksklusif yang tidak bisa Anda temuin di tempat lain.</p>
+    </div>
   </div>
-</div>
+</a>
 
+</body>
+</html>
 
 </body>
 </html>
