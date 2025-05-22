a069697b58ece8416e2a3adea1362bc42935d6f7
karina <Putrikarina543@gmail.com>
2025-05-23 01:13:05 +0700

    Update login.php

--- login.php
+++ login.php
@@ -48,18 +48,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="login.css">
     <title>Daftar Akun</title>
 </head>
 <body>
-    <form action="" method="post">
-        <h1> Login Akun</h1>
+<nav class="navbar navbar-expand-lg">
+    <div class="container d-flex justify-content-center">
+      <a class="navbar-brand text-white fw-bold" href="web.php">AUDIORA</a>
+    </div>
+</nav>
+
+<div class="main-content">
+    <form action="" method="post" class="login-card">
+        <h1>Login Akun</h1>
         <label for="username">Username</label>
-        <input type="text" name="username" id="username"><br>
+        <input type="text" name="username" id="username" required><br>
+        
         <label for="password">Password</label>
-        <input type="password" name="password" id="password"><br>
+        <input type="password" name="password" id="password" required><br>
+        
         <input type="checkbox" name="remember" id="remember">
         <label for="remember">Remember Me</label><br>
-        <input type="submit" value="login" name="login">
+        
+        <input type="submit" value="Login" name="login">
     </form>
+</div>
 </body>
+
 </html>
