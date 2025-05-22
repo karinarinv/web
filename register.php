cf342119cc66110199be671d36c78084ea791d8b
karina <Putrikarina543@gmail.com>
2025-05-23 01:14:15 +0700

    Update register.php

--- register.php
+++ register.php
@@ -36,9 +36,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="register.css">
     <title>Daftar Akun</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg">
+    <div class="container d-flex justify-content-center">
+      <a class="navbar-brand text-white fw-bold" href="web.php">AUDIORA</a>
+    </div>
+    </nav>
+
+    <div class="main-content">
+    <form action="" method="post" class="login-card">
     <form action="" method="post">
         <h1> Registrasi Akun</h1>
         <label for="username">Username</label>
@@ -49,5 +60,6 @@
         <input type="password" name="password2" id="password2"><br>
         <input type="submit" value="register" name="register">
     </form>
+</div>
 </body>
 </html>
